28ecb2696eb49d4e9ed10837bbc9dab20504ebcd
Nolan Neeley <nolanneeley@gmail.com>
2018-05-30 05:32:30 -0500

    generate tanks controller with 7 restful actions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'tanks/new'
+  get 'tanks/create'
+  get 'tanks/update'
+  get 'tanks/edit'
+  get 'tanks/destroy'
+  get 'tanks/index'
+  get 'tanks/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
