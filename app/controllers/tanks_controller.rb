94fbd4f52c6f48c092d321bbf1cd16994461263b
Nolan Neeley <nolanneeley@gmail.com>
2018-05-30 06:53:09 -0500

    fill out crud actions and views

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -1,22 +1,53 @@
 class TanksController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
 
   def index
+    @tanks = Tank.all
   end
 
   def show
+    @tank = Tank.find(params[:id])
   end
+
+  def new
+    @tank = Tank.new
+  end
+
+  def edit
+    @tank = Tank.find(params[:id])
+  end
+
+  def create
+    @tank = Tank.new(tank_params)
+
+    if @tank.save
+      redirect_to @tank
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @tank = Tank.find(params[:id])
+
+    if @tank.update(tank_params)
+      redirect_to @tank
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @tank = Tank.find(params[:id])
+    @tank.destroy
+    
+    redirect_to tanks_path
+  end
+
+    private
+
+    def tank_params
+      params.require(:tank).permit(:address)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
 
   root 'tanks#index'
 
-  get 'tanks/new'
-  get 'tanks/create'
-  get 'tanks/update'
-  get 'tanks/edit'
-  get 'tanks/destroy'
-  get 'tanks/index'
-  get 'tanks/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tanks
+
 end
